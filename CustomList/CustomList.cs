a9840f4182c0e3a8e0cd9886db67361c566cf705
Vincent Mackay <mackv42@gmail.com>
2019-07-10 11:48:50 -0700

    init

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList
+{
+    class CustomList<T>
+    {
+        public delegate void Del(T message);
+        private int count;
+        private T[] arr;
+
+        public CustomList() {
+            count = 0;
+        }
+
+        public CustomList(int length)
+        {
+            count = length;
+            arr = new T[length];
+        }
+
+        public CustomList(T[] arr)
+        {
+            int i = 0;
+            try
+            {
+                while (1 == 1)
+                {
+                    T nan;
+                    nan = arr[i];
+                    i++;
+                }
+            } catch(IndexOutOfRangeException E)
+            {
+                this.resize(i);
+                for(int n=0; n<count; n++)
+                {
+                    this.arr[n] = arr[n];
+                }
+            }
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        private void resize(int n)
+        {
+            T[] arr2 = new T[count + n];
+
+            for (int i = 0; i < count; i++)
+            {
+                arr2[i] = arr[i];
+            }
+
+            this.arr = arr2;
+            count += n;
+        }
+
+        public void Add(T item)
+        {
+            this.resize(1);
+            arr[count - 1] = item;
+        }
+
+        public void Add(CustomList<T> add) {
+            this.resize(add.Count());
+            for (int i = add.Count(); i < count; i++)
+            {
+                arr[i] = add[i - add.Count()];
+            }
+        }
+
+        public void Map(Action<T> f )
+        {
+            for(int i=0; i<this.Count(); i++)
+            {
+               f(arr[i]);
+            }
+        }
+
+        public CustomList<T> Filter(Func<T, bool> f)
+        {
+            CustomList<T> ret = new CustomList<T>();
+            for (int i = 0; i < count; i++) { 
+                if (f(arr[i]))
+                {
+                    ret.Add(arr[i]);
+                }
+            }
+
+            return ret;
+        }
+
+        public static CustomList<T> operator+ (CustomList<T> a, CustomList<T> b)
+        {
+            CustomList<T> r = new CustomList<T>(a.Count() + b.Count());
+            for(int i=0; i<a.Count(); i++)
+            {
+                r[i] = a[i];
+            }
+
+            for(int i=0; i<b.Count(); i++) {
+                r[i + a.Count()] = b[i];
+            }
+            return r;
+        }
+
+        public static CustomList<T> Zip(CustomList<T> a, CustomList<T> b)
+        {
+            CustomList<T> ret = new CustomList<T>(a.Count()+b.Count());
+            for(int i=0; i < a.Count()*2; i+=2)
+            {
+                ret[i] = a[i/2];
+            }
+            
+            for(int i=0; i<b.Count()*2; i += 2)
+            {
+                ret[i + 1] = b[i/2];
+            }
+
+            return ret;
+        }
+
+        public T this[int i] {
+            get
+            {
+                return arr[i];
+            }
+
+            set
+            {
+                arr[i] = value;
+            }
+        }
+    }
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CustomList
+{
+   
+    class animal
+    {
+       
+        public string color;
+        private int numLegs;
+        private string sound;
+        public string name;
+        public animal(string name, string sound)
+        {
+            this.sound = sound;
+            this.name = name;
+        }
+
+        public void makeSound()
+        {
+            Console.WriteLine($"The {name} says {sound}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<animal> list = new CustomList<animal>(new animal[]{ new animal("dog", "woof"), new animal("doge", "bork"), new animal("pig", "squueeeel") });
+            CustomList<animal> list2 = new CustomList<animal>(new animal[] { new animal("wombat", "reee"), new animal("seel", "oorf"), new animal("mouse", "eee") });
+            CustomList<animal> animalsZipped = CustomList<animal>.Zip(list, list2);
+
+            Console.Write(list.Count());
+            CustomList<int> c = new CustomList<int>();
+
+            animalsZipped.Map((x) => {
+                x.makeSound();
+            });
+
+            CustomList<animal> filteredList = new CustomList<animal>();
+        }
+    }
+}
